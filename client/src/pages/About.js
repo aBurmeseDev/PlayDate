948f1cf9a644b26d1885a60e07ea183f1d20c8cc
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-06-09 14:58:53 -0700

    add animation

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,10 +7,10 @@ const About = props => (
       <h1>
         Kids<span style={{ color: "yellow" }}>Fun</span>
       </h1>
-      <h5>KidsFun: Fun Family Events & Activities nearby</h5>
+      <h5>KidsFun: Group Chat, Fun Family Events & Activities nearby</h5>
       <p>
         a new cutting-edge web application to help families find fun things to
-        do nearby and real time social chat groups for parents
+        do nearby and real time social group chats for parents
       </p>
     </div>
   </div>
