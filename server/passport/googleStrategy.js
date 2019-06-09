120f6f8848ce0c2d315d162dc3dac583a221cb8a
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-06-08 17:16:32 -0700

    edit google oauth

--- server/passport/googleStrategy.js
+++ server/passport/googleStrategy.js
@@ -1,62 +1,73 @@
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const { User } = require('../db/models/');
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const { User } = require("../db/models/");
 
 const strategy = new GoogleStrategy(
-	{
-		clientID: "961908331426-lvci427bk9qqdnk2qj93hhueta2b0pat.apps.googleusercontent.com",
-		clientSecret: "AXtMBz-R_v8-SPTuHMHdVF_o",
-		callbackURL: '/auth/google/callback'
-	},
-	function(token, tokenSecret, profile, done) {
-		process.nextTick(function() {
-			// testing
-			//console.log('===== GOOGLE PROFILE =======');
-			//console.log(profile);
-			//console.log(profile.emails);
-			//console.log('======== END ===========');
-			// code
-			const { id, displayName, emails, photos } = profile;
+  {
+    clientID:
+      "270783263753-89el6p19cg2a1iu4519fa1pe25u2l3g7.apps.googleusercontent.com",
+    clientSecret: "3mq7t55yLMZyQ2s9sHbgnVDb",
+    callbackURL: "/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function() {
+      // testing
+      //console.log('===== GOOGLE PROFILE =======');
+      //console.log(profile);
+      //console.log(profile.emails);
+      //console.log('======== END ===========');
+      // code
+      const { id, displayName, emails, photos } = profile;
 
-			User.findOne({ email: emails[0].value }).populate('invites').then(userMatch => {
-				// if there is already someone with that email
-				if (userMatch) {
-					//console.log('userMatch:');
-					//console.log(userMatch);
-					if (userMatch.image !== photos[0].value) {
-						User.findOneAndUpdate({_id: userMatch._id}, {'$set': {image: photos[0].value}}, {new: true}).then(result => {return done(null, result);});
-					} else {
-						return done(null, userMatch);
-					}
-				} else {
-					// if no user in our db, create a new user with that email
-					//console.log('====== PRE SAVE =======');
-					//console.log(profile);
-					//console.log('====== post save ....');
-					const newGoogleUser = new User({
-						email: emails[0].value,
-						name: displayName,
-						image: photos[0].value,
-					});
-					// save this user
-					newGoogleUser.save((err, savedUser) => {
-						if (err) {
-							//console.log('Error occurred while saving new google user');
-							console.error(err);
-							return done(null, false);
-						} else {
-							return done(null, savedUser);
-						}
-					}); // closes newGoogleUser.save
-				}
-			}).catch(err => {// handle errors here:
-				if (err) {
-					//console.log('Error occurred while trying to find user by email');
-					console.error(err);
-					return done(null, false);
-				}
-			}); // closes User Query
-		});
-	}
+      User.findOne({ email: emails[0].value })
+        .populate("invites")
+        .then(userMatch => {
+          // if there is already someone with that email
+          if (userMatch) {
+            //console.log('userMatch:');
+            //console.log(userMatch);
+            if (userMatch.image !== photos[0].value) {
+              User.findOneAndUpdate(
+                { _id: userMatch._id },
+                { $set: { image: photos[0].value } },
+                { new: true }
+              ).then(result => {
+                return done(null, result);
+              });
+            } else {
+              return done(null, userMatch);
+            }
+          } else {
+            // if no user in our db, create a new user with that email
+            //console.log('====== PRE SAVE =======');
+            //console.log(profile);
+            //console.log('====== post save ....');
+            const newGoogleUser = new User({
+              email: emails[0].value,
+              name: displayName,
+              image: photos[0].value
+            });
+            // save this user
+            newGoogleUser.save((err, savedUser) => {
+              if (err) {
+                //console.log('Error occurred while saving new google user');
+                console.error(err);
+                return done(null, false);
+              } else {
+                return done(null, savedUser);
+              }
+            }); // closes newGoogleUser.save
+          }
+        })
+        .catch(err => {
+          // handle errors here:
+          if (err) {
+            //console.log('Error occurred while trying to find user by email');
+            console.error(err);
+            return done(null, false);
+          }
+        }); // closes User Query
+    });
+  }
 );
 
 module.exports = strategy;
