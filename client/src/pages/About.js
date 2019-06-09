189c462df6ac705b8021c5c0a717c4157f9c5ad0
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-06-09 11:24:35 -0700

    add about descrption

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,31 +1,27 @@
 import React from "react";
-import { Row, Col} from 'react-bootstrap';
-import './about.css';
-const About = (props) =>
+import { Row, Col } from "react-bootstrap";
+import "./about.css";
+const About = props => (
   <div className="About">
+    <Row>
+      <div>
+        <Col sm={4} md={4} />
+      </div>
+      <div>
+        <Col sm={4} md={4} />
+      </div>
+      <div>
+        <Col sm={4} md={4}>
+          <h1>KidsFun</h1>
 
-  <Row>
-  <div>
-    <Col sm={4} md={4}>
-
-    </Col>
+          <p>
+            is a new cutting-edge mobile technology to help families find fun
+            things to do nearby
+          </p>
+          <h5>KidsFun: Fun Family Events & Activities nearby</h5>
+        </Col>
+      </div>
+    </Row>
   </div>
-  <div>
-    <Col sm={4} md={4}>
-
-    </Col>
-  </div>
-  <div>
-    <Col sm={4} md={4}>
-      <h1>Social Momsies</h1>
-      <p>A place to connect with other Moms</p>
-      <p>Find better activities for the little ones</p>
-
-
-    </Col>
-    </div>
-  </Row>
-</div>
-;
-
+);
 export default About;
